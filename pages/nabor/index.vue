764752bbe9a29e5a9fff967620f1efcfd6c04b68
Jakub Dobry <jakub.dobry8@gmail.com>
2019-07-05 21:00:53 +0200

    Added nabor and navod default page

---
+++ pages/nabor/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <section class="wrapper style1">
+      <div class="container">
+        <h2>Nábory</h2>
+        <nuxt-link tag="li" to="/nabor/helper">
+          <a>Helper</a>
+        </nuxt-link>
+        <nuxt-link tag="li" to="/nabor/builder">
+          <a>Builder</a>
+        </nuxt-link>
+      </div>
+    </section>
+  </div>
+</template>
---
+++ pages/navod/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section class="wrapper style1">
+      <div class="container">
+        <h2>Návody</h2>
+
+        <nuxt-link tag="li" to="/navod/overeni-originalky">
+          <a>Můj nick je již zaregistrovaný</a>
+        </nuxt-link>
+        <nuxt-link tag="li" to="/navod/prihlaseni-bez-hesla">
+          <a>Přihlašování bez hesla/skin na serveru</a>
+        </nuxt-link>
+        <li class="external">
+          <a href="https://heslo.qplay.cz">Změna hesla</a>
+        </li>
+      </div>
+    </section>
+  </div>
+</template>
