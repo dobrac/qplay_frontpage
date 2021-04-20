c4512bf6b3b5cb4b49388e59ded4de4aa659bbc7
Anonymous2479 <43411417+Anonymous2479@users.noreply.github.com>
2021-04-20 21:27:55 +0200

    Message change
    V případě, že bychom Vás vybrali, tak Vás sami zastihneme a pozici Vám nabídneme.

--- pages/nabor/cheathunter.vue
+++ pages/nabor/cheathunter.vue
@@ -16,7 +16,7 @@
               target="_blank"
               >FAQ</a
             >. Záleží i na vaší úspěšnosti při nahlašování, tzn. cheatery
-            nahlašujte pouze při 100% jistotě. Pakliže budete mít dostatečný počet reportů, Hlavní CheatHunter vás sám kontaktuje.
+            nahlašujte pouze při 100% jistotě. V případě, že bychom Vás vybrali, tak Vás sami zastihneme a pozici Vám nabídneme.
           </span>
         </ul>
         <h4><span style="color: #000000">Výhody CheatHuntera</span></h4>
