54dd98756fe4fd084f932c843f1b9b18e2ba92be
_Spetty_ <spetty233@gmail.com>
2021-04-25 20:35:09 +0200

    Text edit

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -7,25 +7,33 @@
           <a href="https://info.qplay.cz" target="_blank" style="">
             <h3>info.qplay.cz (informační portál, ticket systém)</h3>
           </a>
-          Pokud jste dostal ban a myslíte, že neprávem, vytvořte ticket v
-          systému info.qplay.cz. Náš team se na to podívá v co nejkratším možném
-          termínu. Pokud máte problém s odesláním sms, ztratil se Vám nebo Vám
-          nepřišel aktivační kód, či máte jiný problém s VIP, prosíme,
-          kontaktujte nás na info.qplay.cz (odkaz výše)<br />
+          V případě, že byste potřebovali s čímkoliv pomoci, nebo se chtěli na
+          něco zeptat, tak nás můžete skrz daný odkaz výše kdykoliv kontaktovat.
+          Jedná se o podporu, kam se přihlásíte se svými přihlašovacími údaji a
+          následně v záložce "Mé tickety" skrz volbu "Vytvořit nový ticket" si
+          můžete vytvořit tzv. "ticket", kde bude probíhat písemná konverzace s
+          podporou. Součástí vytváření ticketu je i zvolení typu ticketu, zvolte
+          takový typ ticketu, který je Vašemu problému nejbližší.<br />
         </div>
-
         <br />
         <b>Další možnosti kontaktování</b><br />
-        V případě jiných dotazů, návrhu spolupráce atd. nás můžete kontaktovat
-        na emailové adrese <b>support@qplay.cz</b>. Na dotazy ohledně žádostí o
-        unban či problémů s platbou nemusí být na tomto emailu odpovídáno, pro
-        tento druh dotazů prosím využijte informační portál. Pokud máte problém
-        s přístupem do informačního portálu, můžete nás samozřejmě kontaktovat
-        na této emailové adrese. Nicméně doporučujeme si nejdříve přečíst naší
-        sekci FAQ, kde se nachází většina odpovědí na otázky hráčů (
-        <a href="/faq"> FAQ </a>).
-
-        <br /><br /><br />
+        V případě nefunkčnosti info panelu nás můžete kontaktovat na emailové
+        adrese <b>support@qplay.cz</b>. Například na dotazy ohledně žádostí o
+        unban či problémů s platbou v případě funkčního info panelu nemusí být
+        na tomto emailu odpovídáno, pro tento druh dotazů prosím využijte
+        informační portál
+        <a href="https://info.qplay.cz" target="_blank" style="">
+          (info.qplay.cz)</a
+        >. Pokud máte problém s přístupem do informačního portálu, tak si
+        ideálně založte nový účet na
+        <a href="https://registrace.qplay.cz" target="_blank">
+          registrace.qplay.cz</a
+        >
+        a skrz ten nás následně na info panelu kontaktujte. Doporučujeme i před
+        samotném kontaktování jakékoliv podpory si nejdříve přečíst naší sekci
+        FAQ, kde se nachází většina odpovědí na otázky hráčů (<a href="/faq">
+          FAQ </a
+        >).<br /><br /><br />
         <b> Instagram: </b>
         <a href="https://www.instagram.com/qplaycz/" target="_blank">
           <b>@qplaycz</b>
