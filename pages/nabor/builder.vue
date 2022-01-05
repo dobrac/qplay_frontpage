1ebb59e581f22e950d6a8c210406fd6ded2a5c0f
Ralfcz <zichr67@gmail.com>
2022-01-05 20:43:28 +0100

    Reworked Builder application page

--- pages/nabor/builder.vue
+++ pages/nabor/builder.vue
@@ -9,33 +9,45 @@
         <ul>
           <span style="color: #000000">
             Se svými herními údaji se přihlásíte na
-            <a href="https://info.qplay.cz/" target="_blank">Informační Panel</a
-            >. Kliknete na sekci "Mé tickety" a následně si stačí vytvořit nový
+            <a href="http://info.qplay.cz/" target="_blank">Informační Panel</a>
+            . Kliknete na sekci "Mé tickety" a následně si stačí vytvořit nový
             ticket s typem "Žádost o Buildera", kde vám bude poskytnuto více
             informací.
           </span>
         </ul>
         <h4><span style="color: #000000">Výhody Buildera</span></h4>
-        <ul>
-          <li>
-            <span style="color: #000000">Vlastní veškeré VIP výhody.</span>
-          </li>
-          <li>
-            <span style="color: #000000">
-              Může se připojit do plného týmu na minihře MiniAnnihilation a
-              Crystals.
-            </span>
-          </li>
-        </ul>
+        <li>
+          <span style="color: #000000">
+            Vlastní veškeré MASTER VIP výhody a vybrané LEGEND VIP výhody.
+          </span>
+        </li>
+        <li>
+          <span style="color: #000000">
+            Může se připojit do plného týmu na minihře MiniAnnihilation a
+            Crystals.
+          </span>
+        </li>
+        <li>
+          <span style="color: #000000">
+            Jednou za měsíc může udělat svou velkou akci na Discordu, kam obdrží zdarma
+            odměny do soutěže.
+          </span>
+        </li>
+        <li>
+          <span style="color: #000000">
+            Může pořádat menší akce na Discordu v "QPlay Stage".
+          </span>
+        </li> 
+        <br>
         <h4><span style="color: #000000">Odměna za práci Buildera</span></h4>
-        <ul>
+        <li>
           <span style="color: #000000">
             Každý měsíc obdrží určitou sumu coinů za počet postavených map.
           </span>
-        </ul>
+        </li>
         <li>
           <span style="color: #000000">
-            Za odvedenou práci obdrží zpravidla 1x Basic VIP.
+            Za odvedenou práci obdrží každý měsíc zpravidla 1x Basic VIP.
           </span>
         </li>
         <li>
@@ -59,13 +71,7 @@
             Po rozhodnutí vedení serveru může za svou odvedenou práci získat
             finanční odměnu.
           </span>
-        </li>
-        <li>
-          <span style="color: #000000">
-            Jednou za dva měsíce může udělat svou akci na Discordu, kam obdrží
-            zdarma odměny do soutěže.
-          </span>
-        </li>
+        </li>               
       </div>
     </section>
   </div>
