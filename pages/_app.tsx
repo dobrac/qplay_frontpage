480eb5c86cbc3fbb6892cb435b69cf2270965902
Jakub Dobrý <jakub.dobry8@gmail.com>
2022-05-06 21:56:45 +0200

    Fix Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,22 @@ import '../styles/main.scss'
 import type {AppProps} from 'next/app'
 import LayoutDefault from "../layout/LayoutDefault";
 import Head from "next/head";
-import Script from "next/script";
+import ReactGA from 'react-ga';
 import CookieConsent from "react-cookie-consent";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 
 function MyApp({Component, pageProps}: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    ReactGA.initialize('UA-60146032-1');
+  }, []);
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [router.pathname, router.query]);
+
   return (
     <LayoutDefault>
       <Head>
@@ -13,7 +25,6 @@ function MyApp({Component, pageProps}: AppProps) {
         <meta name="description" content="QPlay Minecraft Server"/>
         <meta name="viewport" content='width=device-width, initial-scale=1'/>
       </Head>
-      <Script src="/js/google-analytics.js"/>
       <CookieConsent
         location="bottom"
         buttonText="Souhlasím"
