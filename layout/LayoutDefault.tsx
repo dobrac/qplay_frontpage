0ac4a8f53ce17dc724ece03c166568e943f2366d
_Spetty_ <spetty233@gmail.com>
2023-01-06 20:53:30 +0100

    Správná úprava Scrollnav

--- layout/LayoutDefault.tsx
+++ layout/LayoutDefault.tsx
@@ -126,11 +126,11 @@ const LayoutDefault: NextPage<{ children: ReactChild | ReactChild[] }> = ({ chil
   if (navbarShow) {
     Navbar += ' show';
   }
-  let christmas = true;
+  let christmas = false;
   let Scrollnav = 'navbar navbar-expand-lg';
   if (offset > 0) {
     Scrollnav += ' sticky';
-  } else if (christmas == true) {
+  } else if (christmas) {
     Scrollnav += ' sticky';
   }
 
