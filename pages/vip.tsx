f5896c565b6d13487a8a6e4b0d780e4bf1271cb6
Robot7769 <jan.robot7@gmail.com>
2023-02-11 23:12:01 +0100

    edit link to store

--- pages/vip.tsx
+++ pages/vip.tsx
@@ -79,8 +79,8 @@ export default function VIP() {
           </h2> */}
           <h3 style={{textAlign: "center"}}>
             <span style={{fontWeight: "bold"}}>Zakoupením VIP skrz{" "}<a
-              href="https://store.qplay.cz/category/328613" target="_blank" rel="noreferrer">store.qplay.cz</a>{" "}
-              lze obdržet bonusové Mystery Crates NAVÍC!</span>
+              href="https://store.qplay.cz/category/314228" target="_blank" rel="noreferrer">store.qplay.cz</a>{" "}
+              lze obdržet bonusové Valentine Crates NAVÍC!</span>
           </h3>
           {/*<h4 style={{textAlign: "center"}}>
           <span><a href="#poukaz VIP" ><Image src={Voucher} /></a></span>
