a508eaa948cd92fee2af53e676003b21c3c24dd8
Jakub Dobrý <jakub.dobry8@gmail.com>
2023-08-11 11:46:27 +0200

    Update changelog entry only once in 60 seconds

--- pages/seznam-zmen/[id].tsx
+++ pages/seznam-zmen/[id].tsx
@@ -111,7 +111,7 @@ export const getStaticProps: GetStaticProps<ChangeLogNewProps, StaticPageParams>
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 10 seconds
-    revalidate: 10, // In seconds
+    revalidate: 60, // In seconds
   }
 }
 
