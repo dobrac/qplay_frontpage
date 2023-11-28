3b5a7ed20ac01baa916df1f1ef1feabf24e6a2d8
Jakub Dobrý <jakub.dobry8@gmail.com>
2023-11-28 10:16:12 +0100

    Add links to minecraft-server-list, craftlist

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,6 +21,16 @@ const Footer: FC<PropsWithChildren<{}>> = ({children}) => {
       <div className="footer-content">
         {children}
       </div>
+      <div>
+        <a href="https://minecraft-server-list.cz" target="_blank" style={{display: "none"}}>Meow</a>
+        <a href="https://minecraft-servery.cz">
+          <img src="https://minecraft-servery.cz/img/icons/icon_default.png" alt="Minecraft Serverlist"
+               style={{display: "none"}}/>
+        </a>
+        <a href="https://craftlist.org/qplay" style={{display: "none"}}>
+          <img src="https://craftlist.org/server-icons/ranks/small/61.png" alt="craftlist rank"/>
+        </a>
+      </div>
     </footer>
   );
 }
